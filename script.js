de668802a5b10b9d8a68f79b1d09889fe5a54f8e
AmeyPank <pankantiameya@gmail.com>
2023-02-16 13:32:05 +0530

    Dark Theme/Mode With transition

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+const toggle = document.getElementById('swap');
+const body = document.querySelector('body');
 
+toggle.addEventListener('click', function(){
+    // this.classList.toggle('bi-moon');
+    if(this.classList.toggle('swap')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+})
