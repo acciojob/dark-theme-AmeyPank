38a28e4ddd09749c4846acfa1de6096c1b74802d
AmeyPank <pankantiameya@.com>
2023-04-14 01:23:03 +0530

    Swap running

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-//your code here
-const toggle = document.getElementById('swap');
-const body = document.querySelector('body');
-
-toggle.addEventListener('click', function(){
-    // this.classList.toggle('bi-moon');
-    if(this.classList.toggle('button_night')){
-        body.style.background = 'white';
-        body.style.color = 'black';
-        body.style.transition = '2s';
-    }else{
-        body.style.background = 'black';
-        body.style.color = 'white';
-        body.style.transition = '2s';
-    }
-})
+function swapTheme(){
+	 var element = document.body;
+        element.classList.toggle("button_night");
+}
