cc4c0bd4b564fbffb51ae2addfc8a19fc2aa7479
AmeyPank <pankantiameya@.com>
2023-04-14 01:37:54 +0530

    Created submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function swapTheme(){
-	 var element = document.body;
-        element.classList.toggle("button_day");
+//your code here
+let button=document.getElementById('swap');
+let body=document.getElementsByTagName('body')
+let main=document.getElementById("app")
+
+function swap(){
+    //body.style.backgroundcolor="dark";
+    // var element = document.body;
+    // element.classList.toggle("dark-mode");
+    if(button.getAttribute("class")==="button_day"){
+    document.body.style.backgroundColor = "dark";
+    button.setAttribute("class", "button_night")
+    main.setAttribute("class","night")
+    }
+    else{
+        document.body.style.backgroundColor = "light";
+        button.setAttribute("class", "button_day")
+        main.setAttribute("class","day") 
+    }
+
+    //console.log("hi")
 }
